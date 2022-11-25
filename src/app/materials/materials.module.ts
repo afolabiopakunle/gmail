46ab79bac24d58d1c91a9e5dcca58be9ebd27525
Afolabi <opakunleafolabi@gmail.com>
2022-11-25 22:59:55 +0100

    material modules added

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
@@ -11,6 +15,10 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   exports: [
     MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
   ]
 })
 export class MaterialsModule { }
